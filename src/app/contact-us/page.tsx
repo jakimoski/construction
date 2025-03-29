6b18493d91fcb72740bb3bec135e70cd8c047206
Vlatko Jakimoski <jakimoski.vlatko@hotmail.com>
2025-03-29 10:39:00 +0100

    contact page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -4,11 +4,11 @@ const ContactUs = dynamic(() => import("@/components/Contact/ConatctUs"), {});
 
 export default function Contact() {
   return (
-    <section className="flex flex-col relative justify-center  min-h-screen p-4 z-0 lg:px-24">
+    <main className="flex flex-col relative justify-center  min-h-screen p-4 z-0 lg:px-24">
       <h1 className="text-4xl lg:text-7xl font-bold text-blue-400 pt-24 pb-8 md:mb-8 text-center uppercase">
         Contact Us
       </h1>
       <ContactUs />
-    </section>
+    </main>
   );
 }
--- src/components/Contact/ConatctUs.tsx
+++ src/components/Contact/ConatctUs.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useRef } from "react";
 import ContactForm from "./ConatctForm";
 import Lottie, { LottieRef } from "lottie-react";
